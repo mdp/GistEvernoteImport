2f817d6385ac02c84cd125d66aa0829d25ca2b4b
mdp <m@mdp.im>
2015-07-24 17:23:58 -0700

    Better error

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# TODO: Handle errors gracefully here
+if [ ! -f ./data/config.yml ]; then
+      echo "Config file not found"
+      echo "Remember to create your config.yml file and place it in ./data"
+      exit 1
+fi
 bundle exec ruby import.rb
